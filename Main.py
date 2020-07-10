8d00a6240893d6a6a8003d2541d8051240db214c
herbetelem <31862899+herbetelem@users.noreply.github.com>
2020-07-10 15:43:44 +0200

    load plus ou moin ok et commencer a mettre les comentaires

--- Main.py
+++ Main.py
@@ -6,32 +6,40 @@ import fileFunction.FunctionMain as FMain
 import fileFunction.FunctionPrint as FPrint
 import fileFunction.FunctionGame as FGame
 import fileFunction.variableClassic as VarC
+import fileFunction.FunctionSave as FSave
 import random
 import os
 clear = lambda: os.system('cls')
 
 
 def Main():
+    # je recupere les variable dans mon fichier txt
+    FSave.loadVarClassic()
     # Call all the function
     # appelle de la fonction pour demander le nom du joueur et print les regles
     VarC.nomJoueur = FPrint.debutDuJeux()
-    #
+    # je m'assure que le joueur ai lu les regle
     question = str(input("Est ce que vous êtes pret ? "))
     while question not in VarC.reponseDebut:
         question = str(input("Prenez votre temps et dite moi quand vous serez prêt ! "))
 
     print()
 
+    # je cree de maniere aleatoire les diferent point de spawn des items
     VarC.itemSlot = FBag.createItemSlot(VarC.itemSlot)
     clear()
+    # j'ajoute les items sur la map
     variableMap.mapInATab = FMap.addItemPointOnMap(variableMap.mapInATab, VarC.itemSlot)
+    # j'affiche la carte
     FMap.printMap(VarC.positionJoueurY, VarC.positionJoueurX, variableMap.mapInATab)
 
     print()
     while VarC.statutParti == "ok":
+        # je verifie si le joueur est au point de victoire avec les 3 clef
         if variableMap.mapInATab[VarC.positionJoueurY][VarC.positionJoueurX] == "@":
             if "clef d'or" in VarC.sac and "clef d'argent" in VarC.sac and "clef de bronze" in VarC.sac:
                 FPrint.gameWin(VarC.nomJoueur)
+            # si il n'a pas les clef je le degage de maniere aleatoire dans un espace vide
             else:
                 VarC.prevMoove = "Vous avez essayé de sortir du jeu sans toute les clefs, \n                                    un singe vous assome et vous traine quelque part dans l'ile"
                 punitionY = random.randint(0, len(variableMap.mapInATab) - 1)
@@ -42,18 +50,24 @@ def Main():
                 VarC.positionJoueurY = punitionY
                 VarC.positionJoueurX = punitionX
                 
+        # je verifie si il est a la position d'un jeu et si oui je lance le jeux
+        #------------------------------------------------------------------------------
         elif variableMap.mapInATab[VarC.positionJoueurY][VarC.positionJoueurX] == "£":
             FGame.jeuxNombreMystere()
         elif variableMap.mapInATab[VarC.positionJoueurY][VarC.positionJoueurX] == "$":
             FGame.jeuxCodeCesar()
         elif variableMap.mapInATab[VarC.positionJoueurY][VarC.positionJoueurX] == "€":
             FGame.jeuxFizzBuzz()
+        #------------------------------------------------------------------------------
+
         else:
+            # je demande l'action du joueur
             action = input("Que souhaitez vous faire ? ")
-            
+            # je verifie que l'action est possible
             while action not in VarC.actionPossible:
                 action = input(f"Vous pouvez {VarC.actionPossible}")
             
+            # j'apelle la fonction qui correspond a l'action du joueur
             if action == "regle":
                 FPrint.printRegle()
             
@@ -69,11 +83,13 @@ def Main():
             elif action == "inventaire":
                 FMain.inventaire()
 
+            # je verifie que le joueur ai encore des stats pour continuer
             if VarC.statFaim <= 0 or VarC.statSoif <= 0 or VarC.statSommeil <= 0:
                 VarC.statutParti = "ko"
-
+        # j'apelle la fonction fin de tour qui imprime la carte et les stats
         FMap.endTurn()
 
+    # si la parti est ko game over
     if VarC.statutParti == "ko":
         FPrint.gameOver()
 
--- fileFunction/FunctionSave.py
+++ fileFunction/FunctionSave.py
@@ -1,11 +1,47 @@
 # coding: utf-8
-variable = []
-with open("C:/Users/PYTHON/Documents/GitHub/Autonomie-projet-1/fileFunction/data.txt", "r", encoding = "utf-8") as map : 
-    displayMap = [line for line in map]
-    for test in displayMap:
-        if len(test) > 2:
-            test = test.replace("\n","")
-            print(test)
+import json
+import fileFunction.variableClassic as VarC
+
 
 def loadVarClassic():
-    
+    varLoad = []
+    with open("C:/Users/PYTHON/Documents/GitHub/Autonomie-projet-1/fileVarTxt/variableClassic.txt", "r", encoding = "utf-8") as file : 
+        fileLoaded = [line for line in file]
+        for lineX in fileLoaded:
+            lineX = lineX.replace("\n", "")
+            varLoad.append(lineX)
+    varLoad[2] = int(varLoad[2])
+    varLoad[3] = int(varLoad[3])
+    varLoad[4] = int(varLoad[4])
+    varLoad[5] = int(varLoad[5])
+    varLoad[6] = int(varLoad[6])
+    varLoad[8] = int(varLoad[8])
+
+    varLoad[7] = varLoad[7].split(" | ")
+    varLoad[12] = varLoad[12].split(" | ")
+    varLoad[13] = varLoad[13].split(" | ")
+    varLoad[14] = varLoad[14].split(" | ")
+    varLoad[15] = varLoad[15].split(" | ")
+
+    varLoad[9] = varLoad[9].split(" | ")
+    compteur = 0
+    for index in varLoad[9]:
+        varLoad[9][compteur] = index.split(" ! ")
+        varLoad[9][compteur][1] = int(varLoad[9][compteur][1])
+        compteur += 1
+    VarC.nomJoueur = varLoad[0]
+    VarC.avatar = varLoad[1]
+    VarC.statSoif = varLoad[2]
+    VarC.statFaim = varLoad[3]
+    VarC.statSommeil = varLoad[4]
+    VarC.positionJoueurY = varLoad[5]
+    VarC.positionJoueurX = varLoad[6]
+    VarC.sac = varLoad[7]
+    VarC.limitSac = varLoad[8]
+    VarC.itemSlot = varLoad[9]
+    VarC.prevMoove = varLoad[10]
+    VarC.statutParti = varLoad[11]
+    VarC.actionPossible = varLoad[12]
+    VarC.directionPossible = varLoad[13]
+    VarC.itemActionPossible = varLoad[14]
+    VarC.reponseDebut = varLoad[15]
