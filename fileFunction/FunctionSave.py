5c90920339c9f166f650646f1d3e264062593a69
herbetelem <31862899+herbetelem@users.noreply.github.com>
2020-07-10 12:21:29 +0200

    add test

---
+++ fileFunction/FunctionSave.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+variable = []
+with open("C:/Users/PYTHON/Documents/GitHub/Autonomie-projet-1/fileFunction/data.txt", "r", encoding = "utf-8") as map : 
+    displayMap = [line for line in map]
+    for test in displayMap:
+        if len(test) > 2:
+            test = test.replace("\n","")
+            print(test)
